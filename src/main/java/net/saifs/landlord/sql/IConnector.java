1cc70711b0a464d5f5c40633f37ea6f84d614df3
Saif <xpyrophoenix21@gmail.com>
2021-07-30 13:16:43 +0100

    1.7 is dogshti

--- src/main/java/net/saifs/landlord/sql/IConnector.java
+++ src/main/java/net/saifs/landlord/sql/IConnector.java
@@ -1,8 +1,4 @@
 package net.saifs.landlord.sql;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 public interface IConnector {
-    Connection getConnection() throws SQLException;
 }
--- src/main/java/net/saifs/landlord/sql/SQLiteConnector.java
+++ src/main/java/net/saifs/landlord/sql/SQLiteConnector.java
@@ -1,44 +1,4 @@
 package net.saifs.landlord.sql;
 
-import net.saifs.landlord.Landlord;
-
-import java.io.File;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class SQLiteConnector implements IConnector {
-    private File file;
-    private Connection connection;
-
-    public SQLiteConnector(String filename) {
-        this.file = new File(Landlord.getInstance().getDataFolder().getAbsolutePath() + File.separator + filename);
-        if (!this.file.exists()) {
-            try {
-                if (!this.file.getParentFile().exists()) {
-                    this.file.getParentFile().mkdirs();
-                }
-                this.file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        try {
-            this.connection = this.openConnection();
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public Connection getConnection() {
-        return this.connection;
-    }
-
-    public Connection openConnection() throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.jdbc.xDriver");
-        String url = "jdbc:sqlite:" + this.file.getAbsolutePath();
-        return DriverManager.getConnection(url);
-    }
+public class SQLiteConnector {
 }
